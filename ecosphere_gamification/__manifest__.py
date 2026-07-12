d81a1b60cb8f0e6a4feff0702fd4f2232d551dfd
Gokul-bit165 <gokul9942786@gmail.com>
2026-07-12 17:44:15 +0530

    Fix gamification views loading order: load hr_employee_views after actions are defined

--- ecosphere_gamification/__manifest__.py
+++ ecosphere_gamification/__manifest__.py
@@ -22,11 +22,11 @@
     'data': [
         'security/ir.model.access.csv',
         'data/badge_data.xml',
-        'views/hr_employee_views.xml',
         'views/xp_ledger_views.xml',
         'views/esg_badge_views.xml',
         'views/esg_challenge_views.xml',
         'views/esg_reward_views.xml',
+        'views/hr_employee_views.xml',
         'views/menu.xml',
         'demo/demo_challenges.xml',
         'demo/demo_rewards.xml',
